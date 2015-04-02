47b68494a1dd46d08d637ea9450c16eadaca1a2d
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-02 11:59:25 +0530

    Add minimal validations for products; pass corresponding tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, presence: true
+	validates :title, uniqueness: true
+	validates :price, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: "URL must be a GIF, JPG or PNG image"
+	}
 end
