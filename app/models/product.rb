5b3acfbcd59083c3aaefd30493768fad0e1edd9d
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-08 18:50:25 +0530

    Ensure that no product is destroyed if corresponding line items exist

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,12 +8,19 @@ class Product < ActiveRecord::Base
 	}
 
 	has_many :line_items
-	before_destroy: :ensure_not_referenced_by_any_line_item
+	before_destroy :ensure_not_referenced_by_any_line_item
 
 	def self.latest
 		Product.order(:updated_at).last
 	end
 
 	private
-		def ensure_not_referenced_by
+		def ensure_not_referenced_by_any_line_item
+			if line_items.empty?
+				return true
+			else
+				errors.add(:base, "Line items present")
+				return false
+			end
+		end
 end
