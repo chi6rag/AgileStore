f7a4196dd42f1f7c4e0f76c42235ccbf8ccd6044
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-10 10:46:03 +0530

    Add line item quantity; record page visits on store#index

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,6 +2,7 @@ include CurrentCart
 class LineItemsController < ApplicationController
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
   before_action :set_cart, only: :create
+  before_action :set_counter_to_zero, only: [:create]
 
   # GET /line_items
   # GET /line_items.json
@@ -26,9 +27,8 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    session[:counter] = 0
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.find_or_initialize_by(product: product)
+    @line_item = @cart.add_product(product.id)
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
@@ -73,5 +73,10 @@ class LineItemsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
       params.require(:line_item).permit(:product_id, :cart_id)
+    end
+
+    # sets counter to 0
+    def set_counter_to_zero
+      session[:counter] = 0
     end
 end
