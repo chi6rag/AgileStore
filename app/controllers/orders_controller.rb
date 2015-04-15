fbb9d573617dbf3c6025a9ae18c7d577bb021f65
Chirag Aggarwal <chi6rag@gmail.com>
2015-04-15 17:37:07 +0530

    Improve order form; add order form style; resolve failing tests; resolve line_items error

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,8 +34,9 @@ class OrdersController < ApplicationController
     @order.add_line_items_from_cart(@cart)
     respond_to do |format|
       if @order.save
+        Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        format.html { redirect_to @order, notice: 'Thank you for your order!' }
+        format.html { redirect_to store_path, notice: 'Thank you for your order!' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ActiveRecord::Base
 	PAYMENT_TYPES = ['Check', 'Credit Card', 'Purchase Order']
 	validates :name, :email, :address, presence: true
 	validates :pay_type, inclusion: PAYMENT_TYPES
+	has_many :line_items, dependent: :destroy
 
 	def add_line_items_from_cart(cart)
 		cart.line_items.each do |item|
